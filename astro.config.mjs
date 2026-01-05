212776784d6ecd6565ff230ae355513a5202b5c3
yuhiwt <iwata.yuhei@gmail.com>
2026-01-05 22:25:15 +0900

    un

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yuhlog.pages.dev/',
   integrations: [tailwind()],
+
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
